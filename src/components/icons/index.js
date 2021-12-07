4480688601c69347753f5be2fc7e08b0da18f997
myketheguru <77077996+myketheguru@users.noreply.github.com>
2021-12-07 15:58:36 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/components/icons/index.js
@@ -0,0 +1,29 @@
+import LinkIcon from './LinkIcon'
+import CompassIcon from './CompassIcon'
+import StarIcon from './StarIcon'
+import ChatBubbleIcon from './ChatBubbleIcon'
+import ArrowIcon from './ArrowIcon'
+import ArrowLeftIcon from './ArrowLeftIcon'
+import ArrowUpIcon from './ArrowLeftIcon'
+import ArrowDownIcon from './ArrowLeftIcon'
+import ArrowRightIcon from './ArrowRightIcon'
+import BoltIcon from './BoltIcon'
+import UserPlusIcon from './UserPlusIcon'
+import NetflixIcon from './NetflixIcon'
+
+const Icons =  {
+    LinkIcon,
+    CompassIcon,
+    StarIcon,
+    ChatBubbleIcon,
+    ArrowIcon,
+    ArrowLeftIcon,
+    ArrowUpIcon,
+    ArrowDownIcon,
+    ArrowRightIcon,
+    BoltIcon,
+    UserPlusIcon,
+    NetflixIcon,
+};
+
+export default Icons
